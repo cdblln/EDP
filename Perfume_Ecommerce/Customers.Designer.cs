44377c1f0fa35b8fa391842a325c34d21dbe0f99
cdblln <cdbb2022-2745-29144@bicol-u.edu.ph>
2025-05-13 02:08:12 +0800

    added customers

---
+++ Perfume_Ecommerce/Customers.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Register
+{
+    partial class Customers
+    {
+        private void InitializeComponent()
+        {
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.firstName = new System.Windows.Forms.TextBox();
+            this.lastName = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.maleRadio = new System.Windows.Forms.RadioButton();
+            this.femaleRadio = new System.Windows.Forms.RadioButton();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.confPass = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.addUser = new System.Windows.Forms.Button();
+            this.editUser = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(26, 78);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(465, 376);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customers List";
+            // 
+            // searchUser
+            // 
+            this.searchUser.Location = new System.Drawing.Point(360, 52);
+            this.searchUser.Name = "searchUser";
+            this.searchUser.Size = new System.Drawing.Size(131, 20);
+            this.searchUser.TabIndex = 2;
+            this.searchUser.TextChanged += new System.EventHandler(this.searchUser_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Search";
+            // 
+            // firstName
+            // 
+            this.firstName.Location = new System.Drawing.Point(534, 99);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(100, 20);
+            this.firstName.TabIndex = 4;
+            // 
+            // lastName
+            // 
+            this.lastName.Location = new System.Drawing.Point(712, 99);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(100, 20);
+            this.lastName.TabIndex = 5;
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(534, 157);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(278, 20);
+            this.email.TabIndex = 6;
+            // 
+            // maleRadio
+            // 
+            this.maleRadio.AutoSize = true;
+            this.maleRadio.Location = new System.Drawing.Point(543, 334);
+            this.maleRadio.Name = "maleRadio";
+            this.maleRadio.Size = new System.Drawing.Size(48, 17);
+            this.maleRadio.TabIndex = 7;
+            this.maleRadio.TabStop = true;
+            this.maleRadio.Text = "Male";
+            this.maleRadio.UseMnemonic = false;
+            this.maleRadio.UseVisualStyleBackColor = true;
+            // 
+            // femaleRadio
+            // 
+            this.femaleRadio.AutoSize = true;
+            this.femaleRadio.Location = new System.Drawing.Point(645, 334);
+            this.femaleRadio.Name = "femaleRadio";
+            this.femaleRadio.Size = new System.Drawing.Size(59, 17);
+            this.femaleRadio.TabIndex = 8;
+            this.femaleRadio.TabStop = true;
+            this.femaleRadio.Text = "Female";
+            this.femaleRadio.UseVisualStyleBackColor = true;
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(534, 210);
+            this.pass.Name = "pass";
+            this.pass.PasswordChar = '●';
+            this.pass.Size = new System.Drawing.Size(278, 20);
+            this.pass.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(534, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "First Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(709, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Last Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(534, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Email";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(534, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Password";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(534, 246);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Confirm Password";
+            // 
+            // confPass
+            // 
+            this.confPass.Location = new System.Drawing.Point(534, 262);
+            this.confPass.Name = "confPass";
+            this.confPass.PasswordChar = '●';
+            this.confPass.Size = new System.Drawing.Size(278, 20);
+            this.confPass.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(534, 308);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Gender";
+            // 
+            // addUser
+            // 
+            this.addUser.Location = new System.Drawing.Point(543, 405);
+            this.addUser.Name = "addUser";
+            this.addUser.Size = new System.Drawing.Size(75, 23);
+            this.addUser.TabIndex = 17;
+            this.addUser.Text = "Add User";
+            this.addUser.UseVisualStyleBackColor = true;
+            this.addUser.Click += new System.EventHandler(this.addUser_Click);
+            // 
+            // editUser
+            // 
+            this.editUser.Location = new System.Drawing.Point(712, 405);
+            this.editUser.Name = "editUser";
+            this.editUser.Size = new System.Drawing.Size(75, 23);
+            this.editUser.TabIndex = 18;
+            this.editUser.Text = "Edit User";
+            this.editUser.UseVisualStyleBackColor = true;
+            this.editUser.Click += new System.EventHandler(this.editUser_Click);
+            // 
+            // Customers
+            // 
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.editUser);
+            this.Controls.Add(this.addUser);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.confPass);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.femaleRadio);
+            this.Controls.Add(this.maleRadio);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGrid);
+            this.Name = "Customers";
+            this.Size = new System.Drawing.Size(862, 600);
+            this.Load += new System.EventHandler(this.Customers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox searchUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox firstName;
+        private System.Windows.Forms.TextBox lastName;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.RadioButton maleRadio;
+        private System.Windows.Forms.RadioButton femaleRadio;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox confPass;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button addUser;
+        private System.Windows.Forms.Button editUser;
+    }
+}
---
+++ Perfume_Ecommerce/Customers.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Register
+{
+    public partial class Customers : UserControl
+    {
+        private const string Server = "localhost";
+        private const string Database = "perfume_ecom";
+        private const string Username = "root";
+        private const string Password = "12345";
+        private const string Port = "3306";
+
+        private MySqlConnection connection;
+        private string _originalEmail; // Add this variable to store the original email
+
+        public Customers()
+        {
+            InitializeComponent();
+            this.Load += Customers_Load;  // This hooks the correct Load event handler
+        }
+        private void dataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGrid.Rows[e.RowIndex];
+
+                firstName.Text = row.Cells["first_name"].Value.ToString();
+                lastName.Text = row.Cells["last_name"].Value.ToString();
+                email.Text = row.Cells["email"].Value.ToString();
+                _originalEmail = email.Text; // Store the original email when the cell is clicked
+                string gender = row.Cells["gender"].Value.ToString();
+
+                maleRadio.Checked = (gender == "Male");
+                femaleRadio.Checked = (gender == "Female");
+            }
+        }
+
+
+        private void Customers_Load(object sender, EventArgs e)  // This should match the hooked event
+        {
+            string connStr = $"server={Server};port={Port};uid={Username};pwd={Password};database={Database};";
+            connection = new MySqlConnection(connStr);
+
+            try
+            {
+                connection.Open();
+                string query = "SELECT first_name, last_name, email, gender FROM customers"; // Make sure the table name is correct
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                dataGrid.DataSource = table; // Assumes dataGrid is your DataGridView
+
+                dataGrid.CellClick += dataGrid_CellClick;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading customer data: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        private void ClearForm()
+        {
+            firstName.Text = "";
+            lastName.Text = "";
+            email.Text = "";
+            pass.Text = "";
+            confPass.Text = "";
+            maleRadio.Checked = false;
+            femaleRadio.Checked = false;
+
+            // Enable password fields for add user
+            pass.Enabled = true;
+            confPass.Enabled = true;
+
+            // Do not clear search box
+        }
+        private void editUser_Click(object sender, EventArgs e)
+        {
+            string gender = maleRadio.Checked ? "Male" : (femaleRadio.Checked ? "Female" : "");
+            string first = firstName.Text.Trim();
+            string last = lastName.Text.Trim();
+            string newEmail = email.Text.Trim();  // Get the new email from the textbox
+            string originalEmail = _originalEmail; // Retrieve the stored original email
+
+            if (string.IsNullOrEmpty(originalEmail))
+            {
+                MessageBox.Show("Original email not found. Please select a user from the grid first.");
+                return;
+            }
+
+            // Ensure the fields are not empty
+            if (string.IsNullOrWhiteSpace(first) || string.IsNullOrWhiteSpace(last) || string.IsNullOrWhiteSpace(newEmail))
+            {
+                MessageBox.Show("Please fill out all fields.");
+                return;
+            }
+
+            // Check if the new email already exists in the database (if it's different from the original email)
+            if (newEmail != originalEmail)
+            {
+                string checkEmailQuery = "SELECT COUNT(*) FROM customers WHERE email = @Email";
+                using (MySqlCommand checkEmailCmd = new MySqlCommand(checkEmailQuery, connection))
+                {
+                    checkEmailCmd.Parameters.AddWithValue("@Email", newEmail);
+
+                    try
+                    {
+                        connection.Open();
+                        int emailExists = Convert.ToInt32(checkEmailCmd.ExecuteScalar());
+                        if (emailExists > 0)
+                        {
+                            MessageBox.Show("The email is already taken by another user.");
+                            return; // Stop further execution if the email is already taken
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error checking email: " + ex.Message);
+                        return;
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+
+            // Proceed with the update query
+            string updateQuery = "UPDATE customers SET first_name = @First, last_name = @Last, gender = @Gender, email = @NewEmail WHERE email = @OriginalEmail";
+
+            using (MySqlCommand cmd = new MySqlCommand(updateQuery, connection))
+            {
+                cmd.Parameters.AddWithValue("@First", first);
+                cmd.Parameters.AddWithValue("@Last", last);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+                cmd.Parameters.AddWithValue("@NewEmail", newEmail); // Always set the new email
+                cmd.Parameters.AddWithValue("@OriginalEmail", originalEmail); // Use original email for WHERE clause
+
+                try
+                {
+                    connection.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("User updated successfully.");
+                        ClearForm(); // Assuming you have a method to clear the form fields
+                        RefreshTable(); // Assuming you have a method to refresh the data grid or list
+                    }
+                    else
+                    {
+                        MessageBox.Show("No changes were made. Please verify the details.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error updating user: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+        private void searchUser_TextChanged(object sender, EventArgs e)
+        {
+            string keyword = searchUser.Text.Trim();
+            string query = $"SELECT first_name, last_name, email, gender FROM customers WHERE first_name LIKE @Keyword OR last_name LIKE @Keyword OR email LIKE @Keyword";
+
+            using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection))
+            {
+                adapter.SelectCommand.Parameters.AddWithValue("@Keyword", "%" + keyword + "%");
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGrid.DataSource = table;
+            }
+        }
+        private void RefreshTable()
+        {
+            string query = "SELECT first_name, last_name, email, gender FROM customers";
+            using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection))
+            {
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGrid.DataSource = table;
+            }
+        }
+
+
+
+
+        private void LoadCustomerData()
+        {
+            try
+            {
+                string connStr = $"server={Server};port={Port};uid={Username};pwd={Password};database={Database};";
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    connection.Open();
+                    string query = "SELECT first_name, last_name, email, gender FROM customers";
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dataGrid.DataSource = table;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load data: " + ex.Message);
+            }
+        }
+
+
+        private void addUser_Click(object sender, EventArgs e)
+        {
+            string gender = maleRadio.Checked ? "Male" : (femaleRadio.Checked ? "Female" : "");
+            string first = firstName.Text.Trim();
+            string last = lastName.Text.Trim();
+            string emailText = email.Text.Trim();
+            string password = pass.Text.Trim();
+            string confirmPassword = confPass.Text.Trim();
+
+            // Validation
+            if (string.IsNullOrWhiteSpace(first) || string.IsNullOrWhiteSpace(last) ||
+                string.IsNullOrWhiteSpace(emailText) || string.IsNullOrWhiteSpace(password) ||
+                string.IsNullOrWhiteSpace(confirmPassword))
+            {
+                MessageBox.Show("Please fill out all fields.");
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Passwords do not match.");
+                return;
+            }
+
+            try
+            {
+                connection.Open();
+
+                // Check if email already exists
+                string checkEmail = "SELECT COUNT(*) FROM customers WHERE email = @Email";
+                using (MySqlCommand checkEmailCmd = new MySqlCommand(checkEmail, connection))
+                {
+                    checkEmailCmd.Parameters.AddWithValue("@Email", emailText);
+                    int emailExists = Convert.ToInt32(checkEmailCmd.ExecuteScalar());
+
+                    if (emailExists > 0)
+                    {
+                        MessageBox.Show("User with this email already exists.");
+                        return;
+                    }
+                }
+
+                // Check if full name already exists
+                string checkName = "SELECT COUNT(*) FROM customers WHERE first_name = @First AND last_name = @Last";
+                using (MySqlCommand checkNameCmd = new MySqlCommand(checkName, connection))
+                {
+                    checkNameCmd.Parameters.AddWithValue("@First", first);
+                    checkNameCmd.Parameters.AddWithValue("@Last", last);
+                    int nameExists = Convert.ToInt32(checkNameCmd.ExecuteScalar());
+
+                    if (nameExists > 0)
+                    {
+                        MessageBox.Show("A user with the same first and last name already exists.");
+                        return;
+                    }
+                }
+
+                // Insert user
+                string insertQuery = "INSERT INTO customers (first_name, last_name, email, password, gender) " +
+                                     "VALUES (@First, @Last, @Email, @Password, @Gender)";
+                using (MySqlCommand insertCmd = new MySqlCommand(insertQuery, connection))
+                {
+                    insertCmd.Parameters.AddWithValue("@First", first);
+                    insertCmd.Parameters.AddWithValue("@Last", last);
+                    insertCmd.Parameters.AddWithValue("@Email", emailText);
+                    insertCmd.Parameters.AddWithValue("@Password", password); // plain text
+                    insertCmd.Parameters.AddWithValue("@Gender", gender);
+
+                    insertCmd.ExecuteNonQuery();
+                    MessageBox.Show("User added successfully.");
+                    ClearForm();
+                }
+
+                LoadCustomerData(); // Refresh the grid
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
